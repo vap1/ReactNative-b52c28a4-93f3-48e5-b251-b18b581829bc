7bab6095bcb6b5498098a1f62b0e5f4147e0b80e
vap1 <vaibhavvasant1705@gmail.com>
2023-08-23 12:49:51 +0530

    First Commit

---
+++ src/types/OpportunityTypes.ts
@@ -0,0 +1,85 @@
+
+interface Opportunity {
+  id: number;
+  name: string;
+  status: string;
+  assignedTo: string;
+  description: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface OpportunityDetails {
+  opportunity: Opportunity;
+  tasks: Task[];
+}
+
+interface Task {
+  id: number;
+  name: string;
+  status: string;
+  assignedTo: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface CreateOpportunityRequest {
+  name: string;
+  assignedTo: string;
+  description: string;
+}
+
+interface CreateOpportunityResponse {
+  success: boolean;
+  errorMessage?: string;
+}
+
+interface UpdateOpportunityRequest {
+  id: number;
+  name?: string;
+  status?: string;
+  assignedTo?: string;
+  description?: string;
+}
+
+interface UpdateOpportunityResponse {
+  success: boolean;
+  errorMessage?: string;
+}
+
+interface CreateTaskRequest {
+  opportunityId: number;
+  name: string;
+  assignedTo: string;
+}
+
+interface CreateTaskResponse {
+  success: boolean;
+  errorMessage?: string;
+}
+
+interface UpdateTaskRequest {
+  id: number;
+  name?: string;
+  status?: string;
+  assignedTo?: string;
+}
+
+interface UpdateTaskResponse {
+  success: boolean;
+  errorMessage?: string;
+}
+
+export type {
+  Opportunity,
+  OpportunityDetails,
+  Task,
+  CreateOpportunityRequest,
+  CreateOpportunityResponse,
+  UpdateOpportunityRequest,
+  UpdateOpportunityResponse,
+  CreateTaskRequest,
+  CreateTaskResponse,
+  UpdateTaskRequest,
+  UpdateTaskResponse,
+};
